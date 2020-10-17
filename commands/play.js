86bb2f26e1610afb014e835eea54bc5e553bad4f
Dylan Edwards <dylanmedwards0@gmail.com>
2020-10-17 14:25:49 -0400

    update

--- commands/play.js
+++ commands/play.js
@@ -11,7 +11,6 @@ description:"Plays music",
  async execute(message, args){
   if(message.member.voice.channel){
    const connection = await message.member.voice.channel.join();
-    connection();
     const dispatcher = await connection.play(ytdl(`${args}`, { quality: 'highestaudio' }));
     dispatcher.on('start', () => {
          message.channel.send(`${args} is now playing`);
