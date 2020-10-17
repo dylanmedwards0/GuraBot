b291d62e406d8cea4024dacec0eca4a68d1298a2
Dylan Edwards <dylanmedwards0@gmail.com>
2020-10-17 17:22:20 -0400

    change

--- commands/change.js
+++ commands/change.js
@@ -7,15 +7,16 @@ module.exports ={
     usage: '<Gura, Ame, or Korone>',
     description: "Changes GuraBot to another vtuber",
     execute(message, args){
-        if(args.shift().toLowerCase() === 'gura'){
+        args = args.shift().toLowerCase();
+        if(args === 'gura'){
             role.edit({name: 'GuraBot', color: "#B0E0E6"});
            bot.setUsername("GuraBot");
            bot.setAvatar('./gura_icon.jpg');
-        } else if(args.shift().toLowerCase() === 'ame'){
+        } else if(args === 'ame'){
             role.edit({name: 'AmeBot', color: "FFE4B5"});
             Discord.ClientUser.setUsername("AmeBot");
             client.setAvatar('./ame_icon.jpg');
-        } else if(args.shift().toLowerCase() === 'korone'){
+        } else if(args === 'korone'){
             role.edit({name: 'KoroneBot', color:"#DEB88"});
             client.setUsername("KoroneBot");
             client.setAvatar('./korone_icon.jpg');
