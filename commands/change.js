2e05ac811a6b596adcb3e0a3e814f8532550d9b2
Dylan Edwards <dylanmedwards0@gmail.com>
2020-10-17 16:58:00 -0400

    change

--- commands/change.js
+++ commands/change.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
-import { user } from "GuraBot.js";
+const client = Discord.ClientUser;
+const role = Discord.Role;
+
 module.exports ={
     name: "change",
     args: true,
@@ -7,18 +9,17 @@ module.exports ={
     description: "Changes GuraBot to another vtuber",
     execute(message, args){
         if(args.shift().toLowerCase() === 'gura'){
-           user.setColor("#B0E0E6");
-           user.setName("GuraBot");
-           user.setAvatar('./gura_icon.jpg');
+            role.edit({name: 'AmeBot', color: "#B0E0E6"});
+           client.setUsername("GuraBot");
+           client.setAvatar('./gura_icon.jpg');
         } else if(args.shift().toLowerCase() === 'ame'){
-
-            user.setColor("FFE4B5");
-            user.setName("AmeBot");
-            user.setAvatar('./ame_icon.jpg');
+            role.edit({name: 'AmeBot', color: "FFE4B5"});
+            client.setUsername("AmeBot");
+            client.setAvatar('./ame_icon.jpg');
         } else if(args.shift().toLowerCase() === 'korone'){
-            user.setColor("#DEB88");
-            user.setName("KoroneBot");
-            user.setAvatar('./korone_icon.jpg');
+            role.edit({name: 'AmeBot', color:"#DEB88"});
+            client.setUsername("KoroneBot");
+            client.setAvatar('./korone_icon.jpg');
         }
     }
 }
