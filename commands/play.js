1eb6f38dac9ceb3eee27171da1a1ef9c11755c60
Dylan Edwards <dylanmedwards0@gmail.com>
2020-10-17 15:41:34 -0400

    fix

--- commands/play.js
+++ commands/play.js
@@ -10,8 +10,11 @@ description:"Plays music",
 
  async execute(message, args){
   if(message.member.voice.channel){
-   const connection = await message.member.voice.channel.join();
-    Discord.VoiceConnection.play('https://www.youtube.com/watch?v=J0a2Prc_MQo', { quality: 'highestaudio' });
+   const connection = await message.member.voice.channel;
+    connection.join().then(connection => {
+        const dispatcher = connection.play('https://www.youtube.com/watch?v=J0a2Prc_MQo');
+
+        process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
+    });
     }
-    }
-  }
+  }}
