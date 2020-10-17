74e8518769ff019ca133bb6fece79b0c389cdd67
Dylan Edwards <dylanmedwards0@gmail.com>
2020-10-17 15:30:23 -0400

    fix

--- commands/play.js
+++ commands/play.js
@@ -11,7 +11,7 @@ description:"Plays music",
  async execute(message, args){
   if(message.member.voice.channel){
    const connection = await message.member.voice.channel.join();
-    Discord.VoiceConnection.play(`${args}`, { quality: 'highestaudio' });
+    Discord.VoiceConnection.play('https://www.youtube.com/watch?v=J0a2Prc_MQo', { quality: 'highestaudio' });
     }
     }
   }
