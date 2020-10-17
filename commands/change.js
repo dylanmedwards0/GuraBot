b896a84d63c0f6d4415a6b63e9445e2c1c3bb3c9
Dylan Edwards <dylanmedwards0@gmail.com>
2020-10-17 16:44:09 -0400

    change

--- commands/change.js
+++ commands/change.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports()={
+module.exports ={
     name: "change",
     args: true,
     usage: '<Gura, Ame, or Korone>',
@@ -10,17 +10,17 @@ module.exports()={
            const role1 = new Discord.ClientUser();
            role1.setColor("#B0E0E6");
            role1.setName("GuraBot");
-           role1.setAvatar('D:/gura_icon.jpg');
+           role1.setAvatar('./gura_icon.jpg');
         } else if(args.shift().toLowerCase() === 'ame'){
             const role1 = new Discord.ClientUser();
             role1.setColor("FFE4B5");
             role1.setName("AmeBot");
-            role1.setAvatar('D:/ame_icon.jpg');
+            role1.setAvatar('./ame_icon.jpg');
         } else if(args.shift().toLowerCase() === 'korone'){
             const role1 = new Discord.ClientUser();
             role1.setColor("#DEB88");
             role1.setName("KoroneBot");
-            role1.setAvatar('D:/korone_icon.jpg');
+            role1.setAvatar('./korone_icon.jpg');
         }
     }
 }
