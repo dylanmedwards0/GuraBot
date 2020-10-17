16940a07978bcba79cce30d19958a43fb2dfabae
Dylan Edwards <dylanmedwards0@gmail.com>
2020-10-17 15:06:14 -0400

    commit

--- GuraBot.js
+++ GuraBot.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const { prefix } = require("./config.json");
 
+
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const cooldowns = new Discord.Collection();
