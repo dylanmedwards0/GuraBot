4853f7e463c9cd6ed3735b36f1cd54ef2ba482ad
Dylan Edwards <dylanmedwards0@gmail.com>
2020-10-17 13:00:30 -0400

    music playing

---
+++ commands/play.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports = {
+name:"play",
+description:"Plays music",
+
+
+
+async execute(message, args){
+    const connection = await message.member.voice.channel.join();
+
+
+
+
+}
+};
