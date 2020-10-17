64b3147e0711f711cd4912791a73dddc86c57f81
Dylan Edwards <dylanmedwards0@gmail.com>
2020-10-17 16:43:14 -0400

    change

---
+++ commands/change.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+module.exports()={
+    name: "change",
+    args: true,
+    usage: '<Gura, Ame, or Korone>',
+    description: "Changes GuraBot to another vtuber",
+    execute(message, args){
+        if(args.shift().toLowerCase() === 'gura'){
+           const role1 = new Discord.ClientUser();
+           role1.setColor("#B0E0E6");
+           role1.setName("GuraBot");
+           role1.setAvatar('D:/gura_icon.jpg');
+        } else if(args.shift().toLowerCase() === 'ame'){
+            const role1 = new Discord.ClientUser();
+            role1.setColor("FFE4B5");
+            role1.setName("AmeBot");
+            role1.setAvatar('D:/ame_icon.jpg');
+        } else if(args.shift().toLowerCase() === 'korone'){
+            const role1 = new Discord.ClientUser();
+            role1.setColor("#DEB88");
+            role1.setName("KoroneBot");
+            role1.setAvatar('D:/korone_icon.jpg');
+        }
+    }
+}
