d513e23c98b8269e338b1c96d531a9e027789f63
Dylan Edwards <dylanmedwards0@gmail.com>
2020-10-18 14:35:09 -0400

    fixes

--- commands/change.js
+++ commands/change.js
@@ -10,29 +10,16 @@ module.exports = {
 
     execute(message, args){
     const client = message.client;
-    let myRole = message.guild.roles.cache.get("766156135647674389");
     if (args[0] == 'gura'){
-     myRole.edit({
-        color: "#B0E0E6",
-        name: "Bot"
-     }).catch(console.error);
-     
      client.user.setAvatar(avatar=`./gura_icon.jpg`);
      message.guild.member(client.user).setNickname(username="GuraBot");
      
    } else if (args[0] == "korone"){
-    myRole.edit({
-        color: "#DEB887"
-     }).catch(console.error);
-     
      client.user.setAvatar(avatar= `./korone_icon.jpg`);
      message.guild.member(client.user).setNickname(username="KoroneBot");
      
    }
      else if (args[0] == "ame"){
-        myRole.edit({
-            color: "#FFE4B5"
-         }).catch(console.error);
 
        client.user.setAvatar(avatar=`./ame_icon.jpg`);
        message.guild.member(client.user).setNickname(nickname= 'AmeBot');
