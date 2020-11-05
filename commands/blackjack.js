053484c038db1db08eedf50b5a2a3453333976b3
Dylan Edwards <dylanmedwards0@gmail.com>
2020-11-04 20:50:29 -0500

    fix

--- commands/blackjack.js
+++ commands/blackjack.js
@@ -8,24 +8,19 @@ cooldown: 0,
 execute(message, args){
 //the lobby
 var blackjackSession = new Array();
+var lobbyUp = false;
 
-
-//creating a game
-if(blackjackSession[0] == undefined && args[0] === "start"){
-    blackjackSession[0] = message.author;
-    message.reply("you've made a blackjack lobby!")
-} else {  
-  message.reply("Wrong Command! Type !join to join a session in play.");
-}
-
-//joining a game
-if(args[0] === "join" && !blackjackSession[0] == undefined){
-    blackjackSession.push(message.author);
-    message.reply("you've joined " + blackjackSession[0] + "'s blackjack lobby!");
-}
-
-
-
+    //creating and joining a game
+    if(blackjackSession[0] == undefined && args[0] === "start" && lobbyUp == false){
+        blackjackSession[0] = message.author;
+        lobbyUp = true;
+        message.reply("you've made a blackjack lobby!")
+    } else if(args[0] === "join" && !blackjackSession[0] == undefined && lobbyUp == true){
+        blackjackSession.push(message.author);
+        message.reply("you've joined " + blackjackSession[0] + "'s blackjack lobby!");
+    } else{  
+    message.reply("Wrong Command! Type !join to join a session in play.");
+    }
 
 }
 
