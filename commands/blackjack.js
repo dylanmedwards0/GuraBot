6166993716b5d4b007958f538b6c01cafffc6db5
Dylan Edwards <dylanmedwards0@gmail.com>
2020-11-04 21:05:18 -0500

    fix

--- commands/blackjack.js
+++ commands/blackjack.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 let lobbyUp = false;
+let userData = JSON.parse(fs.readFileSync("./userData.json", "utf8"));
 module.exports = {
 args: true,
 usage: "<start, join>",
