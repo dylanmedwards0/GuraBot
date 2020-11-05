8385d3df62528bf872fda678a92ed61d39feb52e
Dylan Edwards <dylanmedwards0@gmail.com>
2020-11-04 21:16:30 -0500

    fix

--- commands/blackjack.js
+++ commands/blackjack.js
@@ -1,7 +1,5 @@
 const Discord = require("discord.js");
-const fs = require("fs");
 let lobbyUp = false;
-let userData = JSON.parse(fs.readFileSync("./userData.json", "utf8"));
 module.exports = {
 args: true,
 usage: "<start, join>",
@@ -15,21 +13,16 @@ var blackjackSession = new Array();
 
     //creating and joining a game
     if(blackjackSession[0] == undefined && args[0] === "start" && lobbyUp == false){
-        if(userData[message.author.id + message.guild.id].money < 50){
-            userData[message.author.id + message.guild.id].money += 50;
-        }
         blackjackSession[0] = message.author;
         lobbyUp = true;
         message.reply("you've made a blackjack lobby!")
-    } else if(args[0] === "join" && blackjackSession[0] != undefined && lobbyUp == true && blackjackSession[0] != message.author){
-        if(userData[message.author.id + message.guild.id].money < 50){
-            userData[message.author.id + message.guild.id].money += 50;
-        }
+    } else if(args[0] === "join" && blackjackSession[0] != undefined && lobbyUp == true && blackjackSession[0] != message.author ){
         blackjackSession.push(message.author);
         message.reply("you've joined " + blackjackSession[0] + "'s blackjack lobby!");
     } else{  
     message.reply("Wrong Command! Type !join to join a session in play.");
     }
+    
 
 }
 
