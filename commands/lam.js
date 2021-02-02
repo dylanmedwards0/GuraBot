3689f1ff225692bce35140f5594fda79f2d2df8b
Dylan Edwards <dylanmedwards0@gmail.com>
2021-02-01 21:52:47 -0500

    Lam command

---
+++ commands/lam.js
@@ -0,0 +1,31 @@
+module.exports = {
+    name: "lam",
+    description: "Gura summons her pegging machine",
+    execute(message, args){
+
+        let min = 0;
+        let max = 4;
+        let randomNum = Math.floor(Math.random() * (max - min))  + min;
+
+        if(randomNum == 1){
+        message.channel.send("IT'S PEGGING TIME", {
+            file: "https://i.imgur.com/Qi0DKaZ.jpg"
+        });
+        message.delete({ timeout: 1000 });
+
+    } else if(randomNum == 2){
+        message.channel.send("IT'S PEGGING TIME", {
+            file: "https://i.imgur.com/DebhtPe.png"
+        });
+        message.delete({ timeout: 1000 });
+    } else if(randomNum == 3){
+            message.channel.send("IT'S PEGGING TIME", {
+            file: "https://i.imgur.com/gQxl43a.jpg"});
+            message.delete({ timeout: 1000 });
+    } else if(randomNum == 4){
+        message.channel.send("IT'S PEGGING TIME", {
+        file: "https://i.imgur.com/yXKFSfq.jpg"});
+        message.delete({ timeout: 1000 });
+}
+}
+};
