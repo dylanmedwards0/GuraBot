6aafe7075d0e0cbd321b2c52594ae99f8829711b
Dylan Edwards <dylanmedwards0@gmail.com>
2021-02-02 12:00:56 -0500

    boss command

--- commands/boss.js
+++ commands/boss.js
@@ -9,10 +9,10 @@ module.exports = {
     execute(message, args){
         
         let any = ["Ancient_Guardian", "Ancient_Herald", "Bee_Queen", "Crab_King", "Iron_Hulk", "Lord_of_the_Fruit_Flies", "Malbatross", "Palm_Treeguard", "Pugalisk", "Quacken", "Queen_Womant", "Reanimated_Skeleton", "Shadow_Pieces", "Spider_Queen", "Toadstool", "Treeguard"];
-        let summer = ["Antlion", "Dragonfly",];
+        let summer = ["Antlion", "Dragonfly"];
         let autumn = ["Bearger",];
-        let winter = ["Deerclops", "Klaus", "Sealnado",];
-        let spring = ["Moose/Goose", "Tiger_Shark", ];
+        let winter = ["Deerclops", "Klaus", "Sealnado"];
+        let spring = ["Moose/Goose", "Tiger_Shark"];
         let sendAny = "";
 
 
@@ -20,10 +20,30 @@ module.exports = {
         for(i = 0; i < any.length; i++)
         {
         sendAny = sendAny + `\n` + `https://dontstarve.fandom.com/wiki/${any[i]}`;
+        }   
+    } else if (args == "summer"){
+        for(i = 0; i < summer.length; i++)
+        {
+        sendAny = sendAny + `\n` + `https://dontstarve.fandom.com/wiki/${summer[i]}`;
         }
-
-        message.channel.send(sendAny);
-        
+    } else if (args == "autumn"){
+        for(i = 0; i < autumn.length; i++)
+        {
+        sendAny = sendAny + `\n` + `https://dontstarve.fandom.com/wiki/${autumn[i]}`;
+        }
+    } else if (args == "winter"){
+        for(i = 0; i < winter.length; i++)
+        {
+        sendAny = sendAny + `\n` + `https://dontstarve.fandom.com/wiki/${winter[i]}`;
+        }
+    } else if (args == "spring"){
+        for(i = 0; i < spring.length; i++)
+        {
+        sendAny = sendAny + `\n` + `https://dontstarve.fandom.com/wiki/${spring[i]}`;
+        }
     }
+    message.channel.send(sendAny);
+    message.delete({ timeout: 1000 });
+
 }
 }
