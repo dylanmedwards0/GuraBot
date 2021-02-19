090c29a6eb2814570e4cf81392f2080f847adc6a
Dylan Edwards <dylanmedwards0@gmail.com>
2021-02-19 17:31:14 -0500

    boomer

---
+++ commands/boomer.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "boomer",
+    description: "Gura summons Za Booma",
+    execute(message, args){
+        let l1 = new MessageAttachment("https://media1.tenor.com/images/e358b95f34119a88c93ca90d19a80a03/tenor.gif?itemid=15072629");
+
+        message.channel.send(`OKIE DOKIE BOOMER`, l1);
+        message.delete({ timeout: 1000 });
+}
+}
